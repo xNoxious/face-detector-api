650ae624c3db18eaee184a3ebd1303117a2c9138
xNoxious <11569981+xNoxious@users.noreply.github.com>
2022-03-25 18:23:56 +0200

    Add database script

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const profile = require('./controllers/profile');
 const db = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true
     }
 });
